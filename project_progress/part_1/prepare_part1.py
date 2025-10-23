06a8860af47d0b6477575fc56dbc2067883d7de2
DavidRomero12321 <david.romero03@estudiant.upf.edu>
2025-10-23 15:02:55 +0200

    Fix preprocess import

--- project_progress/part_1/prepare_part1.py
+++ project_progress/part_1/prepare_part1.py
@@ -1,4 +1,4 @@
 import argparse, sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))  # add project root
 
-from myapp.search.preprocess import preprocess_jsonl
+from project_progress.part_1.preprocess import preprocess_jsonl
