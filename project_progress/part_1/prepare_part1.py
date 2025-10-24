ad2753b54c71a2ea93a7074fe844dd7314207281
DavidRomero12321 <david.romero03@estudiant.upf.edu>
2025-10-24 09:35:11 +0200

    Change in the import

--- project_progress/part_1/prepare_part1.py
+++ project_progress/part_1/prepare_part1.py
@@ -1,4 +1,4 @@
 import argparse, sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))  # add project root
 
-from project_progress.part_1.preprocess import preprocess_jsonl
+from preprocess import preprocess_jsonl
